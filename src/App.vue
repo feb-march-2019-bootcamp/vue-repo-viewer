4cd3602b68266b9ea6f27926515d4254630e8de1
Omar jaff <omarjaff95@gmail.com>
2019-03-07 09:28:06 +0300

    Used bootstrap css

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,29 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6">
-          <div class="form-group">
-            <input type="text" 
-              @keypress.enter="loadRepositories"
-              class="form-control" v-model="username">
-          </div>
-          <div class="form-group">
-            <button @click="loadRepositories" class="btn btn-primary btn-block">
-              Load Repisitories
-            </button>
+          <div class="card border-secondary">
+            <div class="card-header bg-primary">
+              <h3>Choose username</h3>
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-12">
+                  <div class="form-group">
+                    <input type="text" 
+                      @keypress.enter="loadRepositories"
+                      class="form-control" v-model="username">
+                  </div>
+                  <div class="form-group">
+                    <button @click="loadRepositories" class="btn btn-primary btn-block">
+                      Load Repisitories
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card-footer">
+              This is footer
+            </div>
           </div>
         </div>
         <div class="col-lg-6">
@@ -24,10 +38,10 @@
 
 <script>
 import 'bootstrap/dist/css/bootstrap.css';
-
 import HelloWorld from './components/HelloWorld.vue'
 import RepoList from './components/RepoList.vue'
 import Axios from 'axios'
+import $ from 'jquery';
 
 export default {
   name: 'app',
@@ -37,10 +51,22 @@ export default {
   },
   data: () => ({
     username: 'octocat',
-    repos: []
+    repos: ['abc']
   }),
-  mounted() {
+  beforeCreate() {
+    console.log({...this})
+  },
+  created() {
     this.loadRepositories();
+    console.log({...this})
+  },
+  mounted() {
+    window.VueInstance = this;
+    $('input').hide();
+    $('input').fadeIn(2000);
+  },
+  updated() {
+    console.log('Updated')
   },
   methods: {
     loadRepositories() {
@@ -63,5 +89,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.box {
+  margin: 2rem;
+  background: orangered;
+  border: yellow solid .4rem;
+  padding: 45%;
 }
 </style>
--- src/components/RepoList.vue
+++ src/components/RepoList.vue
@@ -1,12 +1,42 @@
 <template>
-    <ul class="list-group">
+    <!-- <ul class="list-group">
         <li v-for="(repo, i) in repos"
             class="list-group-item"
             :class="{[liClasses[i % liClasses.length]]: true}"
             :key="repo.id">
             <a v-bind:href="repo.html_url" v-text="repo.name"></a>
         </li>
-    </ul>
+    </ul> -->
+    <div class="table-responsive">
+        <table class="table table-bordered table-striped table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Thumbnail</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="repo in repos" :key="repo.id">
+                    <td v-text="repo.id"></td>
+                    <td v-text="repo.name"></td>
+                    <td>
+                        <div style="width: 3rem">
+                            <img class="img-fluid img-thumbnail rounded" :src="repo.owner.avatar_url" alt="">
+                        </div>
+                    </td>
+                    <th>
+                        <div class="btn-group">
+                            <a class="btn btn-info" 
+                            :href="repo.html_url">Visit</a>
+                            <button class="btn btn-danger">Delete</button>
+                        </div>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
